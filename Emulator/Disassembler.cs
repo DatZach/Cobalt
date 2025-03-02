557b9c11edd3bb8e9c332f5ec65a5f063d33b040
Zach Reedy <paraton@hotmail.com>
2025-03-02 15:49:47 -0500

    WIP: Disassembler works!

--- Emulator/Disassembler.cs
+++ Emulator/Disassembler.cs
@@ -1,16 +1,39 @@
-﻿namespace Emulator
+﻿using System.Text;
+
+namespace Emulator
 {
     public sealed class Disassembler
     {
         private readonly Dictionary<int, MicrocodeRom.Opcode> opcodeMetadata;
+        private readonly Dictionary<int, IReadOnlyList<OperandType>> reverseOperandTable;
         private readonly Machine machine;
 
         public Disassembler(MicrocodeRom microcodeRom, Machine machine)
         {
-            //opcodeMetadata = microcodeRom?.OpcodeMetadata?.ToDictionary(
-            //    x => x.Value.Index,
-            //    x => x.Value
-            //) ?? throw new ArgumentNullException(nameof(microcodeRom));
+            opcodeMetadata = microcodeRom?.OpcodeMetadata?.ToDictionary(
+                x => x.Value.Index | (x.Value.OperandCount == 0 ? 0x8000 : 0),
+                x => x.Value
+            ) ?? throw new ArgumentNullException(nameof(microcodeRom));
+
+            reverseOperandTable = new Dictionary<int, IReadOnlyList<OperandType>>();
+            foreach (var kvp in Microcode.OperandTable)
+            {
+                foreach (var value in kvp.Value)
+                {
+                    var parts = kvp.Key.Split(' ');
+                    var key = ((parts.Length & 0x07) << 8) | value;
+
+                    reverseOperandTable[key] = parts.Select(x => x switch
+                    {
+                        "REG" => OperandType.Reg,
+                        "IMM" => OperandType.Imm,
+                        "SZ[PG:REG+sIMM]" => OperandType.DerefSizePgRegPlusSImm,
+                        "SZ[PG:REG]" => OperandType.DerefSizePgReg,
+                        "SZ[PG:uIMM]" => OperandType.DerefSizePgUImm,
+                        _ => throw new ArgumentOutOfRangeException(nameof(x), x, "Unknown Operand Type Name")
+                    }).ToList();
+                }
+            }
 
             this.machine = machine ?? throw new ArgumentNullException(nameof(machine));
         }
@@ -21,191 +44,153 @@
             // 1 Operand (1OOOOOAA AXXXXXXX) + Flags
             // 2 Operand (0OOOOOAA ABBBXXXX) NO FLAGS
 
-            return "";
+            var iword = machine.ReadWord(segment, offset);
+            var operand0 = machine.ReadWord(segment, (ushort)(offset + 2));
+            var operand1 = machine.ReadWord(segment, (ushort)(offset + 4));
+            offset += 2;
 
-            //var iword = machine.ReadWord(segment, offset);
-            //offset += 2;
+            int opcodeIndex;
+            if ((iword & 0x8F00) == 0)
+                opcodeIndex = ((iword & 0x7000) >> 12) | 0x8000;
+            else
+                opcodeIndex = ((iword & 0x7000) >> 10) | ((iword & 0x00C0) >> 6);
 
-            //int opcodeIndex = (iword & 0x8000) switch
-            //{
-            //    0x8000 => (iword & 0xFC00) >> 10,
-            //    0x0000 => (iword & 0xF000) >> 10,
-            //    _ => -1
-            //};
+            var conditional = (iword & 0x8000) switch
+            {
+                0x0000 => (Conditional)((iword & 0x0F00) >> 8),
+                _ => Conditional.None
+            };
 
-            //var conditional = (iword & 0x8000) switch
-            //{
-            //    0x8000 => (Conditional)((iword & 0x03C0) >> 6),
-            //    0x0000 => (Conditional)((iword & 0x0F00) >> 8),
-            //    _ => Conditional.None
-            //};
+            if (!opcodeMetadata.TryGetValue(opcodeIndex, out var metadata))
+                return $"; UNK {iword:X4} {operand0:X4} {operand1:X4}";
 
-            //if (!opcodeMetadata.TryGetValue(opcodeIndex, out var metadata))
-            //    return $"; UNK {iword:X4}";
+            var opcodeName = metadata.Name;
+            if (metadata.Name is "JMPS" or "JMPL") opcodeName = "JMP";
 
-            //string? operandA = null;
-            //string? operandB = null;
-            //string? operandC = null;
-            //var operand1Type = metadata.OperandCount >= 1 ? (OperandType)((iword & 0x0038) >> 3) : OperandType.None;
-            //var operand2Type = metadata.OperandCount >= 2 ? (OperandType)(iword & 0x0007) : OperandType.None;
-            //OperandType operand3Type = OperandType.None;
-            //var isFlipped = false;
+            var sb = new StringBuilder(80);
+            sb.Append(opcodeName);
+            if (conditional != Conditional.None && conditional != Conditional.Operandless)
+            {
+                sb.Append('.');
+                sb.Append(conditional.ToString().ToUpperInvariant());
+            }
 
-            //if (metadata.OperandCount >= 2)
-            //{
-            //    var operandCombo = metadata.OperandCombinations.FirstOrDefault(x => x.A == operand1Type && x.B == operand2Type);
-            //    if (operandCombo == null)
-            //        return $"; UNK {iword:X4}";
+            sb.Append(' ');
 
-            //    isFlipped = operandCombo.RTL;
-            //}
+            var iT = (iword & 0x8000) >> 15;
+            var iS = (iword & 0x20) != 0 ? 1 : 0;
+            var operandIndex = ((metadata.OperandCount & 0x07) << 8) | (iT << 5) | (iword & 0x1F);
 
-            //if (isFlipped)
-            //    (operand1Type, operand2Type) = (operand2Type, operand1Type);
+            IReadOnlyList<OperandType>? operands = null;
+            if (metadata.OperandCount > 0 && !reverseOperandTable.TryGetValue(operandIndex, out operands))
+                return $"; UNK {iword:X4} {operand0:X4} {operand1:X4}";
 
-            //if (metadata.OperandCount == 3)
-            //{
-            //    operand3Type = operand2Type;
-            //    operand2Type = operand1Type;
-            //}
+            var operandFormat = Microcode.OperandFormats[operandIndex & 0x07];
+            int regIndex = 0, immIndex = 0;
+            for (int k = 0; operands != null && k < metadata.OperandCount; ++k)
+            {
+                var operand = operands[k];
+                int regOperand;
+                short immValue;
 
-            //ushort regWord;
-            //if (IsRegRefOperand(operand1Type) || IsRegRefOperand(operand2Type) || IsRegRefOperand(operand3Type))
-            //{
-            //    if (IsRegRefOperand(operand3Type))
-            //    {
-            //        regWord = machine.ReadWord(segment, offset);
-            //        offset += 2;
-            //    }
-            //    else
-            //    {
-            //        regWord = (ushort)(machine.ReadByte(segment, offset) << 8);
-            //        offset += 1;
-            //    }
-            //}
-            //else
-            //    regWord = 0;
+                if (IsRegRefOperand(operand))
+                {
+                    var idx = (iT << 4) | regIndex;
+                    regOperand = idx switch
+                    {
+                        0x00 => (operand0 & 0xF000) >> 12,
+                        0x01 => (operand0 & 0x0F00) >> 8,
+                        0x02 => (operand0 & 0x00F0) >> 4,
+                        0x10 => (iword & 0x0F00) >> 8,
+                        0x11 => (operand0 & 0xF000) >> 12,
+                        0x12 => (operand0 & 0x0F00) >> 8,
+                        _ => throw new ArgumentOutOfRangeException(nameof(idx), idx, "Illegal RegIndex")
+                    };
 
-            //for (int k = 0; k < metadata.OperandCount; ++k)
-            //{
-            //    OperandType type;
-            //    if (k == 0)
-            //        type = operand1Type;
-            //    else if (k == 1)
-            //        type = operand2Type;
-            //    else if (k == 2)
-            //        type = operand3Type;
-            //    else
-            //        return $"; UNK {iword:X4}";
+                    ++regIndex;
+                }
+                else
+                    regOperand = -1;
 
-            //    var ko = 12 - k * 4;
-            //    var regOperand = (regWord & (0xF << ko)) >> ko;
+                if (IsImmRefOperand(operand))
+                {
+                    int immOffset, immLength;
+                    if (immIndex == 0)
+                    {
+                        immOffset = operandFormat.OffsetA;
+                        immLength = operandFormat.LengthA;
+                    }
+                    else if (immIndex == 1)
+                    {
+                        immOffset = operandFormat.OffsetB;
+                        immLength = operandFormat.LengthB;
+                    }
+                    else
+                        throw new ArgumentOutOfRangeException(nameof(immIndex), immIndex, "Illegal ImmIndex");
 
-            //    string value;
-            //    switch (type)
-            //    {
-            //        case OperandType.Reg:
-            //            value = ParseRegisterName(regOperand);
-            //            break;
+                    var imm = ((long)iword << 32) | ((long)operand0 << 16) | operand1;
+                    var lsbOffset = 48 - immOffset - immLength;
+                    var immMask = (int)Math.Pow(2, immLength) - 1;
+                    immValue = (short)((imm & (immMask << lsbOffset)) >> lsbOffset);
 
-            //        //case OperandType.Imm when conditional != Conditional.fIMM8:
-            //        //{
-            //        //    value = $"0x{machine.ReadWord(segment, offset):X4}";
-            //        //    offset += 2;
-            //        //    break;
-            //        //}
+                    ++immIndex;
+                }
+                else
+                    immValue = -1;
 
-            //        //case OperandType.Imm when conditional == Conditional.fIMM8:
-            //        //{
-            //        //    value = $"0x{machine.ReadByte(segment, offset):X2}";
-            //        //    offset += 1;
-            //        //    break;
-            //        //}
+                var busWidthName = iS == 1 ? "WORD" : "BYTE";
+                string value;
+                switch (operand)
+                {
+                    case OperandType.Reg:
+                        value = ParseRegisterName(regOperand);
+                        break;
 
-            //        case OperandType.DerefWordPgReg:
-            //        case OperandType.DerefBytePgReg:
-            //        {
-            //            var busWidthName = operand1Type == OperandType.DerefWordPgReg ? "WORD" : "BYTE";
-            //            var regName = ParsePgRegIndex(regOperand);
-            //            value = $"{busWidthName} [{regName}]";
-            //            break;
-            //        }
+                    case OperandType.Imm:
+                        value = $"0x{immValue:X4}";
+                        break;
 
-            //        case OperandType.DerefWordPgRegPlusSImm:
-            //        case OperandType.DerefBytePgRegPlusSImm:
-            //        {
-            //            var busWidthName = operand1Type == OperandType.DerefWordPgRegPlusSImm ? "WORD" : "BYTE";
-            //            var regName = ParsePgRegIndex(regOperand);
-            //            var immWidth = 2;//SelectOperandWidth(regOperand);
+                    case OperandType.DerefSizePgReg:
+                    {
+                        var regName = ParsePgRegIndex(regOperand);
+                        value = $"{busWidthName} [{regName}]";
+                        break;
+                    }
 
-            //            short immValue;
-            //            if (immWidth == 1)
-            //                immValue = (short)-machine.ReadByte(segment, offset);
-            //            else
-            //                immValue = (short)-machine.ReadWord(segment, offset);
-            //            offset += (ushort)immWidth;
+                    case OperandType.DerefSizePgRegPlusSImm:
+                    {
+                        var regName = ParsePgRegIndex(regOperand);
 
-            //            if (immValue == 0)
-            //                value = $"{busWidthName} [{regName}]";
-            //            else
-            //            {
-            //                var signStr = (immValue & 0x8000) != 0 ? "" : "+";
-            //                value = $"{busWidthName} [{regName}{signStr}{immValue}]";
-            //            }
-            //            break;
-            //        }
+                        if (immValue == 0)
+                            value = $"{busWidthName} [{regName}]";
+                        else
+                        {
+                            immValue = (short)-immValue;
+                            var signStr = (immValue & 0x8000) != 0 ? "" : "+";
+                            value = $"{busWidthName} [{regName}{signStr}{immValue}]";
+                        }
+                        break;
+                    }
 
-            //        case OperandType.DerefBytePgUImm:
-            //        {
-            //            var segName = ParsePageIndex(regOperand);
-            //            value = $"BYTE [{segName}:0x{machine.ReadWord(segment, offset):X4}]";
-            //            offset += 2;
-            //            break;
-            //        }
+                    case OperandType.DerefSizePgUImm:
+                    {
+                        var segName = ParsePageIndex(regOperand);
+                        value = $"{busWidthName} [{segName}:0x{machine.ReadWord(segment, offset):X4}]";
+                        offset += 2;
+                        break;
+                    }
 
-            //        case OperandType.DerefWordPgUImm:
-            //        {
-            //            var segName = ParsePageIndex(regOperand);
-            //            value = $"WORD [{segName}:0x{machine.ReadWord(segment, offset):X4}]";
-            //            offset += 2;
-            //            break;
-            //        }
+                    default:
+                        value = "<UNK>";
+                        break;
+                }
 
-            //        default:
-            //            value = "";
-            //            break;
-            //    }
+                sb.Append(value);
+                if (k < metadata.OperandCount - 1)
+                    sb.Append(", ");
+            }
 
-            //    if (k == 0)
-            //        operandA = value;
-            //    else if (k == 1)
-            //        operandB = value;
-            //    else if (k == 2)
-            //        operandC = value;
-            //}
-            
-            //if (isFlipped)
-            //{
-            //    if (metadata.OperandCount == 2)
-            //        (operandA, operandB) = (operandB, operandA);
-            //    else if (metadata.OperandCount == 3)
-            //        (operandA, operandB, operandC) = (operandC, operandB, operandA);
-            //}
-
-            //var opcodeName = metadata.Name;
-
-            //if (metadata.Name is "JMPS" or "JMPL") opcodeName = "JMP";
-            
-            //var conditionalName = conditional != Conditional.None ? $".{conditional.ToString().ToUpperInvariant()}" : "";
-
-            //return metadata.OperandCount switch
-            //{
-            //    0 => $"{opcodeName}{conditionalName}",
-            //    1 => $"{opcodeName}{conditionalName} {operandA}",
-            //    2 => $"{opcodeName}{conditionalName} {operandA}, {operandB}",
-            //    3 => $"{opcodeName}{conditionalName} {operandA}, {operandB}, {operandC}",
-            //    _ => $"; UNK {iword:X4}"
-            //};
+            return sb.ToString();
         }
 
         private readonly static string[] Registers =
@@ -215,7 +200,7 @@
         };
         private static string ParseRegisterName(int idx)
         {
-            return Registers[idx];
+            return Registers.ElementAtOrDefault(idx) ?? "??";
         }
 
         private readonly static string[] PgRegs =
@@ -225,7 +210,7 @@
         };
         private static string ParsePgRegIndex(int idx)
         {
-            return PgRegs[idx];
+            return PgRegs.ElementAtOrDefault(idx) ?? "??";
         }
 
         private readonly static string[] Pages =
@@ -235,18 +220,24 @@
         };
         private static string ParsePageIndex(int idx)
         {
-            return Pages[idx];
+            return Pages.ElementAtOrDefault(idx) ?? "??";
         }
-
-        private static int SelectOperandWidth(int index) => (index & 0xC) == 0x4 ? 1 : 2;
 
         private static bool IsRegRefOperand(OperandType operandType)
         {
             return operandType
                 is OperandType.Reg
-                or OperandType.DerefBytePgRegPlusSImm or OperandType.DerefWordPgRegPlusSImm
-                or OperandType.DerefBytePgReg or OperandType.DerefWordPgReg
-                or OperandType.DerefBytePgUImm;
+                or OperandType.DerefSizePgRegPlusSImm or OperandType.DerefBytePgRegPlusSImm or OperandType.DerefWordPgRegPlusSImm
+                or OperandType.DerefSizePgReg or OperandType.DerefBytePgReg or OperandType.DerefWordPgReg
+                or OperandType.DerefSizePgUImm or OperandType.DerefBytePgUImm or OperandType.DerefWordPgUImm;
+        }
+
+        private static bool IsImmRefOperand(OperandType operandType)
+        {
+            return operandType
+                is OperandType.Imm
+                or OperandType.DerefSizePgRegPlusSImm or OperandType.DerefBytePgRegPlusSImm or OperandType.DerefWordPgRegPlusSImm
+                or OperandType.DerefSizePgUImm or OperandType.DerefBytePgUImm or OperandType.DerefWordPgUImm;
         }
     }
 }
